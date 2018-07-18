b36112328069d310bb3683c79512a4da0517fa6e
enzoftware <lizama.enzo@gmail.com>
2018-07-18 19:18:53 +0200

    :room: Working in fullscreen

--- app/src/main/java/com/jetpack/enzoftware/kamer/IntroActivity.kt
+++ app/src/main/java/com/jetpack/enzoftware/kamer/IntroActivity.kt
@@ -1,12 +1,19 @@
 package com.jetpack.enzoftware.kamer
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 
 class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        supportActionBar!!.hide()
+
     }
 }
